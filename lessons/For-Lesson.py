f52217a3cc5ad60f69392ce52f5358665b1b83c9
Francesco Vito Giotta <167875380+LemonPower21@users.noreply.github.com>
2025-10-25 14:00:08 +0200

    Add files via upload

---
+++ lessons/For-Lesson.py
@@ -0,0 +1,9 @@
+vett = "Ciao"
+for x in vett:
+    print(x,end="")
+
+#FOR E' CONSIGLIATO QUANDO SI SA IL NUMERO DI ITERAZIONI DA ESEGUIRE
+
+
+# CONTENITORE-1 : range(a,b,c)  da a a b-1  c= step   #SIMILE ALLO SLICING DELLE STRINGHE
+# FUNZIONE ENUMERATE for (index,value) in enumerate(vett):  LISTA DI TUPLE (index,value)
