203fb3ebb291e93b9ef48828e09a8fb49928d121
Francesco Vito Giotta <167875380+LemonPower21@users.noreply.github.com>
2026-06-08 10:00:05 +0200

    Update S14e1.m

--- ExamMATLAB/Classe-S14/S14e1.m
+++ ExamMATLAB/Classe-S14/S14e1.m
@@ -3,7 +3,7 @@ x = linspace(4, 6, 8);
 A = vander(x);
 [U, S, V] = svd(A);
 
-%RANGO 5 QUINDI BUTTO A ZERO AUTOVALORI SU da 6 fino alla fine
+%RANGO 5 QUINDI BUTTO A ZERO singular value SU da 6 fino alla fine
 S(6:end, 6:end) = 0;
 An = U * S_k * V';
 risultato = norm(An, 1);
